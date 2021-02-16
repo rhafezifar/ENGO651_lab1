8efa75f5129dc2f5dafa94ab59f53f80de8fbc5e
rayehe <hafezifar.r@gmail.com>
2021-02-16 01:03:01 -0700

    pandas andded + import.py created

---
+++ import.py
@@ -0,0 +1,14 @@
+import os
+import pandas as pd
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+# Check for environment variable
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
+
+# Set up database
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
