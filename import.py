36f975f14b3fd71a0ebd44750c6254532fbeaa6a
rayehe <hafezifar.r@gmail.com>
2021-02-16 01:35:50 -0700

    import.py updated

--- import.py
+++ import.py
@@ -12,3 +12,14 @@ if not os.getenv("DATABASE_URL"):
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
+dataframe = pd.read_csv('books.csv')
+for i in range(len(dataframe)):
+    isbn = dataframe['isbn'][i]
+    title = dataframe['title'][i].replace("'", "''")
+    author = dataframe['author'][i].replace("'", "''")
+    year = dataframe['year'][i]
+    db.execute(f"INSERT INTO books(isbn, title, author, year) values ('{isbn}', '{title}', '{author}', {year})")
+
+db.commit()
+
+print("Done.")
